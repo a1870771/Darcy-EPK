b437565af486c6239dfeeca5a3993d4cd42a77fb
Jack <jackrlarkin2004@gmail.com>
2023-11-21 09:42:30 +1030

    i think i fixed it

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -59,8 +59,6 @@ export default function ContactUs() {
         }),
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type",
         },
         method: "POST",
       });
